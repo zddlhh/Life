36f20e95a93d5f114b9795fcfddc2c29b479c329
Alex.Zhou <zddlhh@gmail.com>
2016-03-28 17:50:38 +0800

    modify

--- js/report.js
+++ js/report.js
@@ -1,5 +1,8 @@
 (function(){
 	$(document).ready(function(){
+		
+		
+		
 		var points = new Array();
 		
 		var Point = function(){
@@ -7,10 +10,11 @@
 		};
 		Point.prototype.X = 0;
 		Point.prototype.Y = 0;
-		function init(){
+		
+		function refresh(){
 			var containerWidth = document.body.clientWidth;
 			var containerHeight = document.body.clientHeight;
-			var divMain = $('divMain');
+			//var divMain = $('divMain');
 			//divMain.width(containerWidth);
 			//divMain.height(containerHeight);
 			
@@ -21,8 +25,8 @@
 			//cvs.width(containerWidth);
 			//cvs.height(containerHeight);
 			
-			var width = Math.floor(cvs.width()); //.css("width");
-			var height = Math.floor(cvs.height());//.css("height");
+			var width = cvs.width(); //.css("width");
+			var height = cvs.height();//.css("height");
 			
 			var widthCount = 12;
 			var heightCount = 80;
@@ -31,33 +35,47 @@
 			var perWidth = Math.floor(width / widthCount);
 			var perHeight = Math.floor(height / heightCount);
 			var ctx = cvs[0].getContext("2d");
-			ctx.strokeStyle = "red";
+			ctx.strokeStyle = "darkgray";
 			
-			for(var i = 0; i < heightCount; i++){
+			var maxWidth = perWidth * widthCount;
+			var maxHeight = perHeight * heightCount;
+			for(var i = 0; i <= heightCount; i++){
 				ctx.save();
 				ctx.translate(0.5, 0.5);
 				ctx.lineWidth = 1;
 				ctx.beginPath();
 				ctx.moveTo(0, perHeight * i);
-				console.log(perHeight * i);
+				//console.log(perHeight * i);
 				ctx.lineTo(width, perHeight * i);
 				ctx.closePath();
 				ctx.stroke();
 				ctx.restore();
 			}
 			
-			for(var j = 0; j < widthCount; j++) {
+			for(var j = 0; j <= widthCount; j++) {
+				ctx.save();
 				ctx.lineWidth = 1;
+				ctx.translate(0.5, 0.5);
 				ctx.beginPath();
 				ctx.moveTo(perWidth * j, 0);
-				console.log(perWidth * j);
+				//console.log(perWidth * j);
 				ctx.lineTo(perWidth * j, height);
 				ctx.closePath();
 				ctx.stroke();
+				ctx.restore();
 			}
 		}
 		
-		init();
-
+		$(window).resize(resizeCanvas);
+		
+		function resizeCanvas() {
+			var canvas = $('#cvsMain');
+			canvas.attr("width", $(window).get(0).innerWidth);
+			canvas.attr("height", $(window).get(0).innerHeight);
+			refresh();
+			//context.fillRect(0, 0, canvas.width(), canvas.height());
+		};
+		   
+		resizeCanvas();
 	});
 })();
